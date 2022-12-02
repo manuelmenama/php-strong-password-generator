95e4a589960ae1645961c658297a59366166ca64
manuelmenama <manuel.mena090589@gmail.com>
2022-12-02 15:38:46 +0100

    first

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+
+  <title>Strong Password Generator 2000</title>
+</head>
+<body>
+
+  <div class="container mt-5">
+
+    
+    <div class="row">
+      
+      <div class="col-8 offset-2">
+
+        <div class="card mb-3">
+          <div class="card-body">
+            <p>Inserisci</p>
+          </div>
+        </div>
+        
+        <div class="card text-center">
+          <div class="card-header">
+          <h1>Strong Password Generator 2000</h1>
+        </div>
+        <div class="card-body">
+          
+        </div>
+        <div class="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
+
+      </div>
+
+    </div>
+
+
+  </div>
+  
+</body>
+</html>
